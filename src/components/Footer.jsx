07a5c95096047d8f41e8a8be3ca09582bb16adaa
chirag1192004 <chirag1192004@gmail.com>
2025-10-17 18:40:34 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -22,7 +22,7 @@ const Footer = () => {
                     <div>
                         <h4 style={{ fontWeight: 700, marginBottom: '10px', color: 'var(--color-secondary)' }}>Get in Touch</h4>
                         <p style={{ color: 'var(--color-text-light)', marginBottom: '8px', display: 'flex', alignItems: 'center' }}><FontAwesomeIcon icon={faEnvelope} style={{ fontSize: '1.2rem', color: 'var(--color-primary)', marginRight: '15px' }} /> chirag11092004@gmail.com</p>
-                        <p style={{ color: 'var(--color-text-light)', marginBottom: '15px', display: 'flex', alignItems: 'center' }}><FontAwesomeIcon icon={faGlobe} style={{ fontSize: '1.2rem', color: 'var(--color-primary)', marginRight: '15px' }} /> New Delhi, India</p>
+                        <p style={{ color: 'var(--color-text-light)', marginBottom: '15px', display: 'flex', alignItems: 'center' }}><FontAwesomeIcon icon={faGlobe} style={{ fontSize: '1.2rem', color: 'var(--color-primary)', marginRight: '15px' }} /> Remote , India</p>
                         <div style={{ marginTop: '10px' }}>
                             <a href="https://linkedin.com/in/chirag-garg-profile" target="_blank" rel="noopener noreferrer" style={{ fontSize: '1.2rem', color: 'var(--color-primary)', marginRight: '15px', textDecoration: 'none' }}><FontAwesomeIcon icon={faLinkedin} /></a>
                             <a href="https://github.com/chirag1192004" target="_blank" rel="noopener noreferrer" style={{ fontSize: '1.2rem', color: 'var(--color-primary)', marginRight: '15px', textDecoration: 'none' }}><FontAwesomeIcon icon={faGithub} /></a>
